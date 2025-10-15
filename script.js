24d3fa36646926df189b016c565ca493f75f241b
VirkMehpreet <virkmehpreet@gmail.com>
2025-10-15 16:32:17 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
-
+const student_names = ["Mehpreet Kaur", "Bhavroop Kaur", "kwkdvbliuahjnb", ";ndvr;i", "aeilfjgv"];
+const nameListDiv = document.getElementById("names");
+nameListDiv.innerHTML = student_names.join("<br>");
