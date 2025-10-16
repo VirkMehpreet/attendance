4da7ac872e8dab59a33663ba2b820647a63c30b5
VirkMehpreet <virkmehpreet@gmail.com>
2025-10-16 11:47:06 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 const student_names = ["Mehpreet Kaur", "Bhavroop Kaur", "kwkdvbliuahjnb", ";ndvr;i", "aeilfjgv"];
 const namesDiv = document.getElementById("names");
-namesDiv.innerHTML = student_names.join("<br>");
+
+student_names.forEach(name => {
+  const label = document.createElemtent("label");
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  checkbox.name = "attendance";
+  checkbox.value = name;
+  
+// Add text after the checkbox
+  label.appendChild(checkbox);
+  label.appendChild(document.createTextNode(" " + name));
+
+  // Add the label to the page, and a line break
+  nameList.appendChild(label);
+  nameList.appendChild(document.createElement("br"));
+  
+});
